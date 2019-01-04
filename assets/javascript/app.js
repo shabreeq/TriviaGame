e0e59ad4e8936618b971386e0828c686f26b038f
ShaBree Quinones <shabreeq@gmail.com>
2019-01-03 22:37:35 -0500

    saving

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+$("#start").on("click" ,function(){
+  console.log("you did it")
+})
+
+var questions = [{
+  question: "Which woman in the office rejected Michael's kiss?",
+  answers: ["Pam", "Jan", "Meredith", "Brushing his teeth?"],
+  correctAnswer: "Pam"
+}, {
+  question: "What is Erin's real name?",
+  answers: ["Alice", "Melly", "Kelly", "Brushing her teeth?"],
+  correctAnswer: "Kelly"
+},{
+  question: "What did Dwight buy after returning to the mall with Jim and Andy?",
+  answers: ["Beets", "New glasses", "A pewter wizard statutte holding a crystal ball", "Brushing his teeth?"],
+  correctAnswer: "Pam"
+},{
+  question: "Fill in the blank. They call it Scranton, the blank city.",
+  answers: ["Coal", "Electric", "Dopest", "Toothbrush"],
+  correctAnswer: "Electric"
+}
+
+]
